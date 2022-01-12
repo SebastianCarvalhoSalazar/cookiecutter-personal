6c3870da1afe6ab860806baa874975ca829e34e2
Sebastian Carvalho Salazar <sebastian.carvalho2596@gmail.com>
2022-01-12 11:07:41 -0500

    Initial Commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import subprocess
+
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a GIT repository...{RESET_ALL}")
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit: Do it by Cookiecutter'])
+
+print(f"{MESSAGE_COLOR}The beginning of yout destiny is defined now! Create and have fun!{RESET_ALL}")
